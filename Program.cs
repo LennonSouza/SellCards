d0c615dd575371b90062e741aee4e3e268c859f9
Fabio Cappi <fabiocappi1998@gmail.com>
2021-05-30 21:28:40 -0300

    Titlte Rename

--- Program.cs
+++ Program.cs
@@ -43,7 +43,7 @@ namespace SellCards {
                     password = accSpl[1];
 
                     Logger.info("Processing {0}. {1}/{2}", login, ++counter, allAccounts.Length);
-                    Console.Title = $"[{counter}/{allAccounts.Length}] -- SteamGameBuyStore -- AccountsLoad: {allAccounts.Length} -- MaFilesLoad: {allMafiles.Count}";
+                    Console.Title = $"[{counter}/{allAccounts.Length}] -- SellCards -- AccountsLoad: {allAccounts.Length} -- MaFilesLoad: {allMafiles.Count}";
                     if (!allMafiles.ContainsKey(login)) {
                         Logger.error(login + " mafile not found");
                         continue;
